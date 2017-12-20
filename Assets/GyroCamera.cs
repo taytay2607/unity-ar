710c6cff511d338c0b8a268b7eebf492ba66c881
Naruepanat <cstaytay@hotmail.com>
2017-12-20 22:56:49 +0700

    calculating distance based on rotation

--- Assets/GyroCamera.cs
+++ Assets/GyroCamera.cs
@@ -11,6 +11,9 @@ public class GyroCamera : MonoBehaviour {
     private Transform wouldObj;
     private float startY;
 
+    [SerializeField]
+    private Transform zoomObj;
+
 	// Use this for initialization
 	void Start () {
         gyroSupport = SystemInfo.supportsGyroscope;
@@ -39,7 +42,21 @@ public class GyroCamera : MonoBehaviour {
         transform.localRotation = gyro.attitude * rotfix;
 	}
 
-    void resetGyroRotation() {
+    public void resetGyroRotation() {
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 500)) {
+            Vector3 hitPoint = hit.point;
+            hitPoint.y = 0;
+
+            float z = Vector3.Distance(Vector3.zero, hitPoint);
+            zoomObj.localPosition = new Vector3(0f, zoomObj.localPosition.y, Mathf.Clamp(z, 2f, 10f));
+        }
+
         startY = transform.eulerAngles.y;
         wouldObj.rotation = Quaternion.Euler(0f, startY, 0f);
     }
