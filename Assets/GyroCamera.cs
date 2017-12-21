fbb736f6d88f356102589d41f876ec40b78fadc8
Naruepanat <cstaytay@hotmail.com>
2017-12-21 14:28:14 +0700

    fix ar

--- Assets/GyroCamera.cs
+++ Assets/GyroCamera.cs
@@ -11,9 +11,6 @@ public class GyroCamera : MonoBehaviour {
     private Transform wouldObj;
     private float startY;
 
-    [SerializeField]
-    private Transform zoomObj;
-
 	// Use this for initialization
 	void Start () {
         gyroSupport = SystemInfo.supportsGyroscope;
@@ -42,21 +39,7 @@ public class GyroCamera : MonoBehaviour {
         transform.localRotation = gyro.attitude * rotfix;
 	}
 
-    public void resetGyroRotation() {
-        int x = Screen.width / 2;
-        int y = Screen.height / 2;
-
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 500)) {
-            Vector3 hitPoint = hit.point;
-            hitPoint.y = 0;
-
-            float z = Vector3.Distance(Vector3.zero, hitPoint);
-            zoomObj.localPosition = new Vector3(0f, zoomObj.localPosition.y, Mathf.Clamp(z, 2f, 10f));
-        }
-
+    void resetGyroRotation() {
         startY = transform.eulerAngles.y;
         wouldObj.rotation = Quaternion.Euler(0f, startY, 0f);
     }
